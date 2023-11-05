e2d06d91912c2865fb59cc996dfc297f0f56b8d1
Luis Torres <luis.torres.dev@outlook.com>
2023-11-05 01:12:50 -0500

    Place automatic Stop Loss when trade orders

---
+++ src/TradingAssistant/TradingAssistant/BinanceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Binance.Net.Enums;
+
+namespace TradingAssistant
+{
+    internal static class BinanceExtensions
+    {
+        internal static OrderSide AsOrderSide(this decimal quantity)
+        {
+            return quantity >= 0 ? OrderSide.Buy : OrderSide.Sell;
+        }
+
+        internal static OrderSide Reverse(this OrderSide side)
+        {
+            return side == OrderSide.Buy ? OrderSide.Sell : OrderSide.Buy;
+        }
+    }
+}
