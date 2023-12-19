1727c590c0cb856cf16094cb12b32956fedde64a
Luis Torres <luis.torres.dev@outlook.com>
2023-12-18 23:58:24 -0500

    Wrap task for updating take profit

---
+++ src/TradingAssistant/TradingAssistant/UpdateTakeProfitTask.cs
@@ -0,0 +1,49 @@
+﻿namespace TradingAssistant
+{
+    public class UpdateTakeProfitTask
+    {
+        private readonly Func<decimal, CancellationToken, Task> _action;
+        private decimal _price;
+        private readonly Task _task;
+        private CancellationTokenSource? _cancellationTokenSource;
+
+        public UpdateTakeProfitTask(decimal entryPrice, Func<decimal, CancellationToken, Task> action)
+        {
+            _price = entryPrice;
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+            _cancellationTokenSource = new CancellationTokenSource();
+            _task = Task.Factory.StartNew(RunActionAsync, TaskCreationOptions.LongRunning);
+        }
+
+        private async Task RunActionAsync()
+        {
+            while (_cancellationTokenSource is not null)
+            {
+                if (_cancellationTokenSource.Token.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                await _action(_price, _cancellationTokenSource.Token);
+                await Task.Delay(1_000, _cancellationTokenSource.Token);
+            }
+        }
+
+        public void UpdatePrice(decimal price)
+        {
+            _price = price;
+        }
+
+        public void Stop()
+        {
+            if (_cancellationTokenSource is not null)
+            {
+                _cancellationTokenSource.Cancel();
+                _task.Wait();
+                _task.Dispose();
+                _cancellationTokenSource.Dispose();
+                _cancellationTokenSource = null;
+            }
+        }
+    }
+}
