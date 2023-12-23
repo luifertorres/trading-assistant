451ca005f273a3a919c623a72719c708890c306e
Luis Torres <luis.torres.dev@outlook.com>
2023-12-23 17:04:28 -0500

    Add logging provider to send logs to Telegram

--- src/TradingAssistant/TradingAssistant/Program.cs
+++ src/TradingAssistant/TradingAssistant/Program.cs
@@ -1,16 +1,26 @@
-﻿namespace TradingAssistant
+﻿using X.Extensions.Logging.Telegram;
+
+namespace TradingAssistant
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             var host = Host.CreateDefaultBuilder(args)
-                .ConfigureServices(services =>
+                .ConfigureLogging((context, builder) =>
                 {
+                    builder.ClearProviders()
+                        .AddTelegram(context.Configuration)
+                        .AddConsole();
+                }).ConfigureServices(services =>
+                {
+                    services.AddSingleton<BinanceService>();
+
+                    services.AddHostedService<SignalsWorker>();
                     services.AddHostedService<StopLossManager>();
                     services.AddHostedService<TakeProfitManager>();
-
-                    services.AddSingleton<BinanceService>();
+                    //services.AddHostedService<SteppedTrailingStopManager>();
+                    services.AddHostedService<TrailingStopManager>();
                 })
                 .Build();
 
