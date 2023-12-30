e7da8c3b72daaa55ee670cf3fb372b2ac667ec81
Luis Torres <luis.torres.dev@outlook.com>
2023-12-30 14:45:57 -0500

    Handle trading signals and place entry limit orders

---
+++ src/TradingAssistant/TradingAssistant/EmaReversionSignalHandler.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+
+namespace TradingAssistant
+{
+    public class EmaReversionSignalHandler(BinanceService binanceService) : INotificationHandler<EmaReversionSignal>
+    {
+        private readonly BinanceService _binanceService = binanceService;
+
+        public async Task Handle(EmaReversionSignal notification, CancellationToken cancellationToken)
+        {
+            var position = await _binanceService.TryGetPositionInformationAsync(notification.Symbol, cancellationToken);
+
+            if (position is not null)
+            {
+                return;
+            }
+
+            var openOrders = await _binanceService.TryGetOpenOrdersAsync(notification.Symbol, cancellationToken);
+
+            if (openOrders.Any())
+            {
+                return;
+            }
+
+            if (!_binanceService.TryGetLeverage(notification.Symbol, out var leverage))
+            {
+                return;
+            }
+
+            var account = await _binanceService.TryGetAccountInformationAsync(cancellationToken);
+
+            if (account is null)
+            {
+                return;
+            }
+
+            const decimal MarginPercentage = 0.1m;
+
+            var riskPercentage = MarginPercentage * leverage;
+            var notional = account.AvailableBalance * riskPercentage / 100;
+            var quantity = notional / notification.EntryPrice;
+
+            await _binanceService.TryPlaceEntryOrderAsync(notification.Symbol,
+                notification.Side,
+                quantity,
+                notification.EntryPrice,
+                cancellationToken);
+        }
+    }
+}
