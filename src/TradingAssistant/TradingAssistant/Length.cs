81180b6a906b693baa34daad44c643f8fa641925
Luis Torres <luis.torres.dev@outlook.com>
2024-08-30 01:43:08 -0500

    Add additional Length values

--- src/TradingAssistant/TradingAssistant/Length.cs
+++ src/TradingAssistant/TradingAssistant/Length.cs
@@ -4,7 +4,10 @@
     {
         public const int Five = 5;
         public const int Eight = 8;
+        public const int Ten = 10;
         public const int Twenty = 20;
+        public const int Fifty = 50;
+        public const int OneHundred = 100;
         public const int TwoHundred = 200;
     }
 }
