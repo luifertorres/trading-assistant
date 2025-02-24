0ab92bcece5a22ccb023a05ccf492fb82d070a02
Luis Torres <luis.torres.dev@outlook.com>
2025-02-23 21:23:51 -0500

    Amplitude extension for Quote

---
+++ src/TradingAssistant/TradingAssistant/QuoteExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Skender.Stock.Indicators;
+
+namespace TradingAssistant
+{
+    internal static class QuoteExtensions
+    {
+        internal static IEnumerable<(DateTime date, double value)> UseAmplitude(this IEnumerable<Quote> quotes)
+        {
+            static (DateTime Date, double) ToAmplitude(Quote quote)
+            {
+                var amplitude = quote.Close > quote.Open
+                    ? (quote.High / quote.Low) - 1
+                    : (quote.Low / quote.High) - 1;
+
+                return (quote.Date, (double)amplitude);
+            }
+
+            return quotes.Select(ToAmplitude);
+        }
+    }
+}
